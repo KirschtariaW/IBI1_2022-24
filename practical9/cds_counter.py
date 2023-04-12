cb316cf423a5f0796dce04d5f86fa3003d5a5402
KirschtariaW <mingrui.22@intl.zju.edu.cn>
2023-04-12 15:43:02 +0800

    add the codes to count the number of the coding sequences

--- practical9/cds_counter.py
+++ practical9/cds_counter.py
@@ -1,3 +1,14 @@
+#count the total number of possible coding sequences formed by this sequence
+
+#create the sequence
 seq='ATGCAATCGACTACGATCTGAGAGGGCCTAA'
 
-
+#use the regular expression
+import re
+#find all the string starting with ATG, ending with ‘TAA’, ‘TAG’, or ‘TGA’.
+a=re.findall(r'ATG.*TAA',seq)
+b=re.findall(r'ATG.*TAG',seq)
+c=re.findall(r'ATG.*TGA',seq)
+#count the number of all the strings
+count=len(a)+len(b)+len(c)
+print(count)
