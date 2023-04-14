c90ccc9466817731dbde5e546c6185c636d5c7f3
KirschtariaW <mingrui.22@intl.zju.edu.cn>
2023-04-14 16:34:12 +0800

    cds_counter.py debug

--- practical9/cds_counter.py
+++ practical9/cds_counter.py
@@ -3,12 +3,12 @@
 #create the sequence
 seq='ATGCAATCGACTACGATCTGAGAGGGCCTAA'
 
-#use the regular expression
-import re
-#find all the string starting with ATG, ending with ‘TAA’, ‘TAG’, or ‘TGA’.
-a=re.findall(r'ATG.*TAA',seq)
-b=re.findall(r'ATG.*TAG',seq)
-c=re.findall(r'ATG.*TGA',seq)
-#count the number of all the strings
-count=len(a)+len(b)+len(c)
-print(count)
+#for re.findall can just find one longest sequence, so it is not good in this task
+
+#the true way to do so
+#use if 
+if seq.count('ATG')== 0: #find ATG exist
+    print("no sequence")
+elif seq.count('ATG')>= 1:
+    count=seq.count('TGA')+seq.count('TAG')+seq.count('TAA') #find the number of each end codon
+    print(count)
