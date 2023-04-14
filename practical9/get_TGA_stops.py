a406de66c07de2de1096bd4940e515dd1e95aad6
KirschtariaW <mingrui.22@intl.zju.edu.cn>
2023-04-14 14:52:50 +0800

    get_TGA_stops.py create

---
+++ practical9/get_TGA_stops.py
@@ -0,0 +1,4 @@
+#read the file, identify sequences which end with the ‘TGA’ stop codon
+# and simplify the sequence name by only putting the gene name.
+# Output the results in a new fasta file TGA_genes.fa.
+# Note that the sequences for each gene run onto several lines.
