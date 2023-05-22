11e37e8ffac3fcb686c1f933865c2348017b38c8
KirschtariaW <mingrui.22@intl.zju.edu.cn>
2023-05-23 07:52:45 +0800

    complete calculatorclass in mortgage

--- practical10/mortgage_affordability_calculator.py
+++ practical10/mortgage_affordability_calculator.py
@@ -4,6 +4,11 @@
 #(2) the purchaser’s annual salary = salary
 # value <= 5*salary
 class calculatorclass:
+    def __init__(self,value,salary):#define the attributes
+        self.value=value
+        self.salary=salary
+    def print_details(self): #to define the print of attributes
+        print(f'value={self.value}, salary={self.salary}')
     def mortgage_affordability_calculator(value, salary):
         '''determines whether an individual can buy a specific house, i.e. the value of the house <= 5*salary'''
         if isinstance(value, (int, float)) and isinstance(salary, (int, float)): #to determine if x ans y are numbers
@@ -18,3 +23,6 @@ class calculatorclass:
 calculatorclass.mortgage_affordability_calculator(18000,35000)
 ##result:
 ##'can buy the house'
+
+a=calculatorclass(18000,35000)
+a.print_details()
