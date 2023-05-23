2052869a786b6be7479dbe84cdf69ced1192fc04
KirschtariaW <mingrui.22@intl.zju.edu.cn>
2023-05-23 08:18:55 +0800

    mortage final modify

--- practical10/mortgage_affordability_calculator.py
+++ practical10/mortgage_affordability_calculator.py
@@ -9,10 +9,10 @@ class calculatorclass:
         self.salary=salary
     def print_details(self): #to define the print of attributes
         print(f'value={self.value}, salary={self.salary}')
-    def mortgage_affordability_calculator(value, salary):
+    def mortgage_affordability_calculator(self):
         '''determines whether an individual can buy a specific house, i.e. the value of the house <= 5*salary'''
-        if isinstance(value, (int, float)) and isinstance(salary, (int, float)): #to determine if x ans y are numbers
-            if value <= 5*salary : #if it is true, can buy
+        if isinstance(self.value, (int, float)) and isinstance(self.salary, (int, float)): #to determine if x ans y are numbers
+            if self.value <= 5*self.salary : #if it is true, can buy
                 return 'can buy the house'
             else : #if it is not true, can not buy
                 return 'can not buy the house'
@@ -20,9 +20,9 @@ class calculatorclass:
             print("Error: value and salary must be numbers.")
 
 #example
-calculatorclass.mortgage_affordability_calculator(18000,35000)
-##result:
-##'can buy the house'
-
 a=calculatorclass(18000,35000)
 a.print_details()
+a.mortgage_affordability_calculator()
+
+
+
